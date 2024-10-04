21f64de405c72924cffbd68c0a72490eac61bb91
sadeen fadel <sadeenfadel@gmail.com>
2024-10-04 17:45:47 +0300

    Revert "freezing123"
    This reverts commit 322078b94beba447d15c319d66204c89abc7c1b1.

--- FLIGHT-SYSTEM/Controllers/WeatherForecastController.cs
+++ FLIGHT-SYSTEM/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace FLIGHT_SYSTEM.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing123","sadeen123","yara", "leen","Abdulaziz","Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing","sadeen123","yara", "leen","Abdulaziz","Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
