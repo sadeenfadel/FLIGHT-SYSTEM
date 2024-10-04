322078b94beba447d15c319d66204c89abc7c1b1
Sadeen <134232396+sadeenfadel@users.noreply.github.com>
2024-10-04 17:45:23 +0300

    freezing123

--- FLIGHT-SYSTEM/Controllers/WeatherForecastController.cs
+++ FLIGHT-SYSTEM/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace FLIGHT_SYSTEM.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing","sadeen123","yara", "leen","Abdulaziz","Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing123","sadeen123","yara", "leen","Abdulaziz","Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
