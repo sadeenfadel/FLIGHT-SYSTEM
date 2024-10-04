10aa2a5de06f887ff181f4cba82e06582573483c
Sadeen <134232396+sadeenfadel@users.noreply.github.com>
2024-10-04 17:12:28 +0300

    Update WeatherForecastController.cs

--- FLIGHT-SYSTEM/Controllers/WeatherForecastController.cs
+++ FLIGHT-SYSTEM/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace FLIGHT_SYSTEM.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freez", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
